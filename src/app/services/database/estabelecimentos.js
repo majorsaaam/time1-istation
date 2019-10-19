211fa5f55a046de7c70d0eb6954914636784b12e
Antonio Bento Pereira <antonio7br@gmail.com>
2019-10-19 10:17:20 -0300

    Estab. Json & Promo. Json

--- src/app/services/database/estabelecimentos.js
+++ src/app/services/database/estabelecimentos.js
@@ -1,18 +1,30 @@
 module.exports = [
     {
         id: 1,
-        nome: "Picolé",
-        cnpj: "11111111111",
-        loja: true
+        nome: "Conveniência Rio",
+        cnpj: "39.752.873/0005-90",
+        loja: true,
+        posto: 2
     }, {
-        id: 1,
-        nome: "Picolé",
-        cnpj: "11111111111",
-        loja: true
+        id: 2,
+        nome: "PetroRio",
+        cnpj: "39.752.873/0005-90",
+        loja: false
     }, {
-        id: 1,
-        nome: "Picolé",
-        cnpj: "11111111111",
-        loja: true
+        id: 3,
+        nome: "Rapid Shop",
+        cnpj: "32.221.921/0217-22",
+        loja: true,
+        posto: 4
+    }, {
+        id: 4,
+        nome: "Concha Oil",
+        cnpj: "12.945.443/1605-81",
+        loja: false
+    }, {
+        id: 5,
+        nome: "Pitanga BR",
+        cnpj: "69.322.551/0234-11",
+        loja: false
     }
 ]
--- src/app/services/database/promocao.js
+++ src/app/services/database/promocao.js
@@ -1,6 +1,18 @@
 module.exports = [
     {
         id: 1,
-        valor: 0.0,
-    }
+        valor: 1.00,
+    },
+    {
+        id: 2,
+        valor: 5.00,
+    },
+    {
+        id: 3,
+        valor: 1.50,
+    },
+    {
+        id: 4,
+        valor: 0.08,
+    },
 ]
