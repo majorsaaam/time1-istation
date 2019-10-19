3c45899e85489cf1af216a60000ddd59416835bc
Samantha Marques <sammy.marques@outlook.com>
2019-10-19 20:33:38 -0300

    Carrinho mock

---
+++ src/app/pagamento-checkout/pagamento-checkout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagamento-checkout',
+  templateUrl: './pagamento-checkout.component.html',
+  styleUrls: ['./pagamento-checkout.component.css']
+})
+export class PagamentoCheckoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
