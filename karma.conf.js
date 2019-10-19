dfe5d7a81c268f7e4379e46d8b086e3b06f280e4
Samantha Marques <sammy.marques@outlook.com>
2019-10-19 05:59:24 -0300

    First commit

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/istation-app'),
+      dir: require('path').join(__dirname, './coverage/time1-istation'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'istation-app'`, () => {
+  it(`should have as title 'time1-istation'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('istation-app');
+    expect(app.title).toEqual('time1-istation');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('istation-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('time1-istation app is running!');
   });
 });
