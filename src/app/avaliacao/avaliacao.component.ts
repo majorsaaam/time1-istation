da50db3ec53bb6f1d64deb05943aa7a107a814f1
JWllan <jwllan@outlook.com>
2019-10-19 23:17:49 -0300

    Avaliação

--- src/app/avaliacao/avaliacao.component.ts
+++ src/app/avaliacao/avaliacao.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
+import { EstabelecimentosService } from '../services/Estabelecimentos/estabelecimentos.service';
+
 @Component({
   selector: 'app-avaliacao',
   templateUrl: './avaliacao.component.html',
@@ -9,9 +11,18 @@ export class AvaliacaoComponent implements OnInit {
   @Input() rating: number;
   @Input() itemId: number;
   @Output() ratingClick: EventEmitter<any> = new EventEmitter<any>();
+  estabServ = new EstabelecimentosService();
+  estabId = 0;
+  estabelecimento;
 
   inputName: string;
   hasBeenRated: boolean = false;
+
+  constructor() {
+    this.estabId = 1;
+    this.estabelecimento = this.estabServ.getById(this.estabId);
+  }
+
   ngOnInit() {
     this.inputName = this.itemId + '_rating';
   }
--- src/app/confirmar-pedido/confirmar-pedido.component.ts
+++ src/app/confirmar-pedido/confirmar-pedido.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EstabelecimentosService } from '../services/Estabelecimentos/estabelecimentos.service';
+
 @Component({
   selector: 'app-confirmar-pedido',
   templateUrl: './confirmar-pedido.component.html',
   styleUrls: ['./confirmar-pedido.component.css']
 })
 export class ConfirmarPedidoComponent implements OnInit {
+  estabServ = new EstabelecimentosService();
   lat: number = -22.8935813;
   lng: number = -43.1989022;
   zoom: number = 15;
-  constructor() { }
+  estabId = 0;
+  estabelecimento;
+
+  constructor() {
+    this.estabId = 1;
+    this.estabelecimento = this.estabServ.getById(this.estabId);
+  }
 
   ngOnInit() {
   }
